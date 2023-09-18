1474c02d1b0e46fd55cc877e8999f960628af5f8
Md Kamran <mdkamran912@gmail.com>
2023-09-18 11:05:20 +0530

    multilevel dropdown

--- resources/views/tutor/tutorprofile.blade.php
+++ resources/views/tutor/tutorprofile.blade.php
@@ -1,181 +1,187 @@
 @extends('tutor.layouts.main')
 @section('main-section')
- <!-- ============================================================== -->
-        <!-- Start right Content here -->
-        <!-- ============================================================== -->
-        <div class="main-content">
-            <style>
-                .listHeader {
-                    display: flex;
-                    justify-content: space-between;
-                }
-            </style>
+<!-- ============================================================== -->
+<!-- Start right Content here -->
+<!-- ============================================================== -->
+<div class="main-content">
+    <style>
+    .listHeader {
+        display: flex;
+        justify-content: space-between;
+    }
 
-            <div class="page-content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12 grid-margin">
-                            <div class="card" style="width: 50rem; margin: 0 auto;">
+    .skillTag {
+        display: flex;
 
-                                <div class=" card-header bg-white">
+    }
 
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <div class="ms-4 d-flex flex-column" style="width: 150px;">
-                                                <img src="{{url('images/tutors/profilepics','/')}}{{ $tutorpd->profile_pic ?? url('images/avatar/default-profile-pic.png')}}"
-                                                    alt="Generic placeholder image" class="img-fluid img-thumbnail mb-2"
-                                                    style="width: 150px; z-index: 1; border-radius: 50%;">
-                
-                                                <a type="button" class="btn btn-outline-dark bg-primary" href="profileupdate"
-                                                    style="z-index: 1;">
-                                                    Edit profile
-                                                </a>
-                
-                                            </div>
-                                        </div>
-                                        <div class="col-9 text-left">
-                                            <h2 style="margin-top: 60px;">{{ $tutorpd->name ?? session('userid')->name}}</h2>
-                                        </div>
-                
+    .skillTag h5 {
+        margin: 10px;
+    }
+    </style>
+
+    <div class="page-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 grid-margin">
+                    <div class="card" style="width: 50rem; margin: 0 auto;">
+
+                        <div class=" card-header bg-white">
+
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <div class="ms-4 d-flex flex-column" style="width: 150px;">
+                                        <img src="{{url('images/tutors/profilepics','/')}}{{ $tutorpd->profile_pic ?? url('images/avatar/default-profile-pic.png')}}"
+                                            alt="Generic placeholder image" class="img-fluid img-thumbnail mb-2"
+                                            style="width: 150px; z-index: 1; border-radius: 50%;">
+
+                                        <a type="button" class="btn btn-outline-dark bg-primary" href="profileupdate"
+                                            style="z-index: 1;">
+                                            Edit profile
+                                        </a>
+
                                     </div>
-                
+                                </div>
+                                <div class="col-9 text-left">
+                                    <h2 style="margin-top: 60px;">{{ $tutorpd->name ?? session('userid')->name}}</h2>
                                 </div>
 
+                            </div>
+
+                        </div>
 
 
-                                <div class="card-body">
 
-                                    <h5>Personal Details</h5>
-                                    <table class="tab table-bordered" width="100%" id="">
-                                        <tr>
-                
-                                            <th>Name:</th>
-                                            <td>{{ $tutorpd->name  ?? session('userid')->name}}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Goal</th>
-                                            <td>{{ $tutorpd->goal  ?? '' }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Qulifications:</th>
-                                            <td>{{ $tutorpd->qualification ?? '' }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Experience:</th>
-                                            <td>{{ $tutorpd->experience ?? session('userid')->mobile }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Certifications</th>
-                                            <td>{{ $tutorpd->certification ?? '' }}</td>
-                                        </tr>
-                
-                                        <tr>
-                                            <th>Mobile:</th>
-                                            <td>{{ $tutorpd->mobile ?? session('userid')->email}}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Secondary Mobile:</th>
-                                            <td>{{ $tutorpd->secondary_mobile ?? '' }}</td>
-                                        </tr>
-                
-                                        <tr>
-                                            <th>Email:</th>
-                                            <td>{{ $tutorpd->email ?? '' }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Headline:</th>
-                                            <td>{{ $tutorpd->headline ?? '' }}</td>
-                                        </tr>
-                                        <tr>
-                                            <th>Area:</th>
-                                            <td>
-                                                @if ($tutorsub ?? '')
-                                                    
-                                                @foreach ($tutorsub as $tutorsu)
-                                                {{ $tutorsu->subject ?? '' }}(£{{ $tutorsu->rate ?? '' }}/hr),
-                                                @endforeach
-                                                @endif
-                                        </td>
-                                                
-                                        </tr>
-                                    </table>
-                                    <br>
-                                    <h5>Acievements</h5>
-                
-                                    <table class="table table-hover table-striped align-middlemb-0 table-responsive">
-                                        <thead>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Details</th>
-                                                <th>Date</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @if ($achievement ?? '')
-                                                
-                                            @foreach ($achievement as $achievement)
-                                            <tr>
-                                                <td class="text-wrap">{{ $achievement->name }}</td>
-                                                <td class="text-wrap">{{ $achievement->description }}</td>
-                                                {{-- <td class="text-wrap">{{$achievement->date}}</td> --}}
-                                                <td>{{ \Carbon\Carbon::parse($achievement->date)->format('j-F-Y') }}</td>
-                                            </tr>
-                                            @endforeach
-                                            @endif
-                                        </tbody>
-                                    </table>
-                
-                                    <br>
-                                    <h5>Reviews</h5>
-                                    <table class="table table-hover table-striped align-middlemb-0 table-responsive">
-                                       
-                                        <tr>
-                                            <th>Subject</th>
-                                            <th>details</th>
-                                            <th>Rating</th>
-                                        </tr>
-                                        @if ($reviews ?? '')
-                                            
-                                        @foreach ($reviews as $reviews)
-                                        
-                                        <tr>
-                                            <td>{{$reviews->subject}}</td>
-                                            <td>{{$reviews->name}}</td>
-                                            
-                                            <td>
-                                                @if($reviews->ratings >=1)
-                                                <span class="fa fa-star checked"></span>
-                                                @endif
-                                                @if($reviews->ratings >=2)
-                                                <span class="fa fa-star checked"></span>
-                                                @endif
-                                                @if($reviews->ratings >=3)
-                                                <span class="fa fa-star checked"></span>
-                                                @endif
-                                                @if($reviews->ratings >=4)
-                                                <span class="fa fa-star checked"></span>
-                                                @endif
-                                                @if($reviews->ratings >=5)
-                                                <span class="fa fa-star checked"></span>
-                                                @endif
-                                            </td>
-                                            
-                                        </tr>
+                        <div class="card-body">
+
+                            <h5>Personal Details</h5>
+                            <table class="tab table-bordered" width="100%" id="">
+                                <tr>
+
+                                    <th>Name:</th>
+                                    <td>{{ $tutorpd->name  ?? session('userid')->name}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Goal</th>
+                                    <td>{{ $tutorpd->goal  ?? '' }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Qulifications:</th>
+                                    <td>{{ $tutorpd->qualification ?? '' }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Experience:</th>
+                                    <td>{{ $tutorpd->experience ?? session('userid')->mobile }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Certifications</th>
+                                    <td>{{ $tutorpd->certification ?? '' }}</td>
+                                </tr>
+
+                                <tr>
+                                    <th>Mobile:</th>
+                                    <td>{{ $tutorpd->mobile ?? session('userid')->email}}</td>
+                                </tr>
+                                <tr>
+                                    <th>Secondary Mobile:</th>
+                                    <td>{{ $tutorpd->secondary_mobile ?? '' }}</td>
+                                </tr>
+
+                                <tr>
+                                    <th>Email:</th>
+                                    <td>{{ $tutorpd->email ?? '' }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Headline:</th>
+                                    <td>{{ $tutorpd->headline ?? '' }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Area:</th>
+                                    <td>
+                                        @if ($tutorsub ?? '')
+
+                                        @foreach ($tutorsub as $tutorsu)
+                                        {{ $tutorsu->subject ?? '' }}(£{{ $tutorsu->rate ?? '' }}/hr),
                                         @endforeach
                                         @endif
-                                    </table>
+                                    </td>
 
-                                    <div>
-                                        <h5>Skills</h5>
-                                       <div class="skillTag">
-                                            <h5><span class="badge bg-primary">Mathematics</span></h5>
-                                            <h5><span class="badge bg-primary">Mathematics</span></h5>
-                                            <h5><span class="badge bg-primary">Mathematics</span></h5>
-                                            <h5><span class="badge bg-primary">Mathematics</span></h5>
-                                            <h5><span class="badge bg-primary">Mathematics</span></h5>
-                                       </div>
-                                    </div>
-                
+                                </tr>
+                            </table>
+                            <br>
+                            <h5>Acievements</h5>
+
+                            <table class="table table-hover table-striped align-middlemb-0 table-responsive">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Details</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if ($achievement ?? '')
+
+                                    @foreach ($achievement as $achievement)
+                                    <tr>
+                                        <td class="text-wrap">{{ $achievement->name }}</td>
+                                        <td class="text-wrap">{{ $achievement->description }}</td>
+                                        {{-- <td class="text-wrap">{{$achievement->date}}</td> --}}
+                                        <td>{{ \Carbon\Carbon::parse($achievement->date)->format('j-F-Y') }}</td>
+                                    </tr>
+                                    @endforeach
+                                    @endif
+                                </tbody>
+                            </table>
+
+                            <br>
+                            <h5>Reviews</h5>
+                            <table class="table table-hover table-striped align-middlemb-0 table-responsive">
+
+                                <tr>
+                                    <th>Subject</th>
+                                    <th>details</th>
+                                    <th>Rating</th>
+                                </tr>
+                                @if ($reviews ?? '')
+
+                                @foreach ($reviews as $reviews)
+
+                                <tr>
+                                    <td>{{$reviews->subject}}</td>
+                                    <td>{{$reviews->name}}</td>
+
+                                    <td>
+                                        @if($reviews->ratings >=1)
+                                        <span class="fa fa-star checked"></span>
+                                        @endif
+                                        @if($reviews->ratings >=2)
+                                        <span class="fa fa-star checked"></span>
+                                        @endif
+                                        @if($reviews->ratings >=3)
+                                        <span class="fa fa-star checked"></span>
+                                        @endif
+                                        @if($reviews->ratings >=4)
+                                        <span class="fa fa-star checked"></span>
+                                        @endif
+                                        @if($reviews->ratings >=5)
+                                        <span class="fa fa-star checked"></span>
+                                        @endif
+                                    </td>
+
+                                </tr>
+                                @endforeach
+                                @endif
+                            </table>
+
+                            <div>
+                                <h5>Skills</h5>
+                                <div class="skillTag">
+                                    <h5><span class="badge bg-primary">Mathematics</span></h5>
+                                    <h5><span class="badge bg-primary">Mathematics</span></h5>
+                                    <h5><span class="badge bg-primary">Mathematics</span></h5>
+                                    <h5><span class="badge bg-primary">Mathematics</span></h5>
+                                    <h5><span class="badge bg-primary">Mathematics</span></h5>
                                 </div>
                             </div>
 
@@ -183,6 +189,9 @@
                     </div>
 
                 </div>
-                <!-- content-wrapper ends -->
+            </div>
 
-@endsection
+        </div>
+        <!-- content-wrapper ends -->
+
+        @endsection
