04228cc323a7529767788f515afaa5f87ac0a051
Md Kamran <mdkamran912@gmail.com>
2023-09-20 15:53:08 +0530

    added new pages
    Attendance

--- resources/views/student/attendance-report.blade.php
+++ resources/views/student/attendance-report.blade.php
@@ -27,6 +27,38 @@
                 <h3>Attendance</h3>
             </div>
 
+            <form id="payment-search" class="">
+
+<div class="form-group mt-">
+    <div class="row">
+       
+        <div class="col-md-3">
+            <label>Start Date</label>
+            <input type="date" name="start_date" class="form-control">
+
+        </div>
+        <div class="col-md-3">
+            <label>End Date</label>
+            <input type="date" name="end_date" class="form-control">
+
+        </div>
+
+        <div class="col-md-3">
+            <label>Status</label>
+            <select type="text" class="form-control" name=""></select>
+
+        </div>
+        <div class="col-md-3" style="margin-top:32px">
+            <button class="btn  btn-primary" style="float:right">Search</button>
+        </div>
+
+
+    </div>
+    
+</div>
+</form>
+<hr>
+
 
             <div class=" table-responsive">
                 <table class="table table-hover table-striped align-middle table-nowrap mb-0 users-table">
@@ -54,68 +86,70 @@
 
 
         </div>
-        <!-- content-wrapper ends -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script>
-        function updateTableAndPagination(data) {
-            // $('#tableContainer').html(data.table);
-            $('.users-table tbody').html(data.table);
-            $('#paginationContainer').html(data.pagination);
-        }
+    </div>
+</div>
+<!-- content-wrapper ends -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+function updateTableAndPagination(data) {
+    // $('#tableContainer').html(data.table);
+    $('.users-table tbody').html(data.table);
+    $('#paginationContainer').html(data.pagination);
+}
 
-        $(document).ready(function() {
-            $('#payment-search').submit(function(e) {
-                e.preventDefault();
-                // alert('test');
-                const page = 1;
-                const ajaxUrl = '{{ route("student.demolist-search") }}'
-                var formData = $(this).serialize();
+$(document).ready(function() {
+    $('#payment-search').submit(function(e) {
+        e.preventDefault();
+        // alert('test');
+        const page = 1;
+        const ajaxUrl = '{{ route("student.demolist-search") }}'
+        var formData = $(this).serialize();
 
-                formData += `&page=${page}`;
+        formData += `&page=${page}`;
 
-                $.ajax({
-                    type: 'post',
-                    url: ajaxUrl, // Define your route here
-                    data: formData,
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
+        $.ajax({
+            type: 'post',
+            url: ajaxUrl, // Define your route here
+            data: formData,
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
 
-                    success: function(data) {
-                        // console.log(data)
-                        updateTableAndPagination(data);
-                    },
-                    error: function(xhr, status, error) {
-                        console.log(xhr.responseText);
-                    }
-                });
-
-            });
-
-
-            $(document).on('click', '#paginationContainer .pagination a', function(e) {
-                e.preventDefault();
-                var formData = $('#payment-search').serialize();
-                const page = $(this).attr('href').split('page=')[1];
-                formData += `&page=${page}`;
-                $.ajax({
-                    type: 'post',
-                    url: '{{ route("student.demolist-search") }}', // Define your route here
-                    data: formData,
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                    success: function(data) {
-                        updateTableAndPagination(data);
-                    },
-                    error: function(xhr, status, error) {
-                        console.log(xhr.responseText);
-                    }
-                });
-            });
-
-
-
+            success: function(data) {
+                // console.log(data)
+                updateTableAndPagination(data);
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+            }
         });
-        </script>
-        @endsection
+
+    });
+
+
+    $(document).on('click', '#paginationContainer .pagination a', function(e) {
+        e.preventDefault();
+        var formData = $('#payment-search').serialize();
+        const page = $(this).attr('href').split('page=')[1];
+        formData += `&page=${page}`;
+        $.ajax({
+            type: 'post',
+            url: '{{ route("student.demolist-search") }}', // Define your route here
+            data: formData,
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success: function(data) {
+                updateTableAndPagination(data);
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+            }
+        });
+    });
+
+
+
+});
+</script>
+@endsection
