c35ee824bc87b82a313069647000439b66c8ed9a
Ranjith Prince <princeaparanjith777@gmail.com>
2023-09-20 15:44:22 +0530

    google meet integration

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\CommonController;
 use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JitsiController;
+use App\Http\Controllers\GoogleCalendarController;
 use App\Http\Controllers\MessagesController;
 use App\Http\Controllers\student\DashboardController;
 use App\Http\Controllers\tutor\TutorDashboardController;
@@ -281,8 +282,10 @@ Route::group(['prefix' => 'tutor', 'middleware' => ['TutorAuthenticate']], funct
     Route::get('liveclass/create', [ZoomClassesController::class, 'create'])->name('tutor.liveclass.create');
     Route::post('liveclass/store', [ZoomClassesController::class, 'store'])->name('tutor.liveclass.store');
     Route::get('getuser', [ZoomClassesController::class, 'getzoomuser'])->name('tutor.liveclass.getuser');
-    Route::get('getclass', [ZoomClassesController::class, 'classlist'])->name('tutor.liveclass.classlist');
-    Route::post('classschedule', [ZoomClassesController::class, 'scheduleclass'])->name('tutor.liveclass.scheduleclass');
+    Route::get('getclass-bkp', [ZoomClassesController::class, 'classlist'])->name('tutor.liveclass.classlist');
+    Route::get('getclass', [GoogleCalendarController::class, 'classlist'])->name('tutor.meet.classlist');
+    Route::post('classschedule-bkp', [ZoomClassesController::class, 'scheduleclass'])->name('tutor.liveclass.scheduleclass-bkp');
+    Route::any('classschedule', [GoogleCalendarController::class, 'scheduleclass'])->name('tutor.liveclass.scheduleclass');
     // Feedback by tutor
     Route::get('feedback', [FeedbackController::class, 'index'])->name('tutor.feedback.list');
     // Message By Tutor
@@ -299,3 +302,4 @@ Route::group(['prefix' => 'tutor', 'middleware' => ['TutorAuthenticate']], funct
 });
 // Create Jitsi Meeting
 Route::get('/jitsi', [JitsiController::class, 'index']);
+Route::get('oauth2callback', [GoogleCalendarController::class,'oauthCallback'])->name('oauth2callback');
