34ea80d8846f44b31caa57ceb6557ccfec048e30
Deepesh2201 <115881125+Deepesh2201@users.noreply.github.com>
2023-09-21 13:43:54 +0530

    update completed classes

--- resources/views/student/completed-classes.blade.php
+++ resources/views/student/completed-classes.blade.php
@@ -76,7 +76,7 @@
                     <thead class="thead-dark ">
                         <tr>
                             <th scope="col">S.No.</th>
-                            <th scope="col">Meeting ID</th>
+                            {{-- <th scope="col">Meeting ID</th> --}}
                             <th scope="col">Status</th>
                             <th scope="col">Subject</th>
                             <th scope="col">Batch</th>
@@ -90,7 +90,7 @@
                         @foreach ($classes as $class)
                         <tr>
                             <td>{{ $loop->iteration }}</td>
-                            <td>{{ $class->meeting_id }}</td>
+                            {{-- <td>{{ $class->meeting_id }}</td> --}}
                             <td>{{ $class->status }}</td>
                             <td>{{ $class->subjects }}</td>
                             <td>{{ $class->batch }}</td>
@@ -98,16 +98,9 @@
                             <td>{{ $class->start_time }}</td>
                             <td>{{ $class->duration }}</td>
                             <td>
-                                @if ($class->is_completed == 0)
-
-                                <a href="{{ $class->join_url }}" target="_blank"><button
-                                        class="btn btn-sm btn-success"><span
-                                            class="fa fa-play-circle "></span>Watch</button></a>
-                                @else
-                                <button class="btn btn-sm btn-success" data-toggle="modal"
-                                    data-target="#openreviewsmodal"
-                                    onclick="openfeedbackmodal('{{$class->class_id}}','{{$class->subject_id}}','{{$class->tutor_id}}')"><span
-                                        class="fa fa-check "></span> Watch</button>
+                                @if ($class->is_completed == 1)
+                                <a href="{{$class->recording_link}}"><button class="btn btn-sm btn-success"><span
+                                        class="fa fa-check "></span> Watch</button></a>
                                 @endforelse
 
                             </td>
