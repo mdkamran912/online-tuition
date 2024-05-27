669f8895eccf2fac392b0de54865051d16f17c93
Deepesh Raj <deepeshinfo22@gmail.com>
2024-05-27 20:13:22 +0530

    added resource page

---
+++ resources/views/front-cms/resources.blade.php
@@ -0,0 +1,92 @@
+@extends('front-cms.layouts.main')
+@section('main-section')
+    <!-- END header -->
+    <section class="bannerSec tutBann">
+        <div class="container-fluid">
+            <div class="tutorHeader">
+                <h1 class="mb-3">
+                    Explore Our Resources
+                </h1>
+
+                <div class="text-center">
+                    <p class="charcol">Stay informed with our expertly written articles.</p>
+
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+    <!-- tutor section -->
+    <section class="tutor-section">
+        <div class="container ">
+
+            <div class="row resourcesTop">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="resourcesBottom">
+                        <h4>Featured</h4>
+                        <h1>Blogs</h1>
+                        <button class="btn search-tutor">Free Trial Class</button>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="row mt-5">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="blog-card">
+                        <img src="img/blog-img-1.png" width="100%" alt="">
+                        <div class="blogDetails">
+                            <span class="feature">
+                                Featured | Student
+                                <span>
+                                    <h5 class="my-2">Tips and Tricks to design a revision timetable for GCSE</h5>
+                                    <p class="bDesc">Education is the best investment one can make in their children’s
+                                        lives.</p>
+                                    <a href="resources_main.html">
+                                        Read more
+                                    </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="blog-card">
+                        <img src="img/blog-img-1.png" width="100%" alt="">
+                        <div class="blogDetails">
+                            <span class="feature">
+                                Featured | Student
+                                <span>
+                                    <h5 class="my-2">Tips and Tricks to design a revision timetable for GCSE</h5>
+                                    <p class="bDesc">Education is the best investment one can make in their children’s
+                                        lives.</p>
+                                    <a href="resources_main.html">
+                                        Read more
+                                    </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="blog-card">
+                        <img src="img/blog-img-1.png" width="100%" alt="">
+                        <div class="blogDetails">
+                            <span class="feature">
+                                Featured | Student
+                                <span>
+                                    <h5 class="my-2">Tips and Tricks to design a revision timetable for GCSE</h5>
+                                    <p class="bDesc">Education is the best investment one can make in their children’s
+                                        lives.</p>
+                                    <a href="resources.html">
+                                        Read more
+                                    </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </section>
+@endsection
