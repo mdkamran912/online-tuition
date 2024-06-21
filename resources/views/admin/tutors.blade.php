ae6a13bbc6d39c04b2385e92aeb180e0c36e8b4a
Deepesh Raj <deepeshinfo22@gmail.com>
2024-06-21 23:09:02 +0530

    Update tutors.blade.php

--- resources/views/admin/tutors.blade.php
+++ resources/views/admin/tutors.blade.php
@@ -104,7 +104,7 @@
                                         <td>£ {{ $ttrlist->rate }}</td>
                                         <td><a href="#" onclick="updatecommission('{{$ttrlist->tutor_id}}','{{$ttrlist->admin_commission}}')"> {{ $ttrlist->admin_commission }}% <span class="badge bg-primary ml-3"> Update</span> </a></td>
                                         <td><a href="tutorslotscheck/{{$ttrlist->tutor_id}}"><span class="badge bg-success ml-3">Check Slots</span> </a></td>
-                                        @if($ttrlist->subject_name)
+                                        {{-- @if($ttrlist->subject_name) --}}
                                         <td>
                                             <div class="form-check form-switch">
                                                 @if ($ttrlist->tutor_status == 1)
@@ -117,9 +117,9 @@
 
                                             </div>
                                         </td>
-                                        @else
+                                        {{-- @else
                                         <td class="text-danger">Profile Not Updated</td>
-                                        @endif
+                                        @endif --}}
 
                                     </tr>
                                 @endforeach
